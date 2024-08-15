185dcf42fa564c0d0b502bae37ce6b5b9022b856
Your Name <user@example.com>
2024-08-15 02:40:57 +0000

    Initial commit with modular structure

--- setup.py
+++ setup.py
@@ -1,25 +1,7 @@
 from setuptools import setup, find_packages
-
 setup(
     name='MOOSE-Scientific-Hypothesis-Discovery',
     version='0.1',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    install_requires=[
-        'dspy',
-        'python-dotenv',
-        'aiohttp',
-        'cachetools',
-        'backoff',
-        'nest_asyncio',
-        'sentence-transformers',
-        'rouge',
-        'openai',
-        'pydub',
-    ],
-    entry_points={
-        'console_scripts': [
-            'moose=main:main',
-        ],
-    },
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().splitlines(),
 )
